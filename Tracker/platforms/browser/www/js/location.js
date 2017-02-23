7931bd6372dbc91d15760ec1b967e12953c5f638
Stylejoe <dueen@outlook.com>
2017-02-23 13:33:41 +0100

    jgeiow

---
+++ Tracker/platforms/browser/www/js/location.js
@@ -0,0 +1,22 @@
+
+    // onSuccess Callback
+    //   This method accepts a `Position` object, which contains
+    //   the current GPS coordinates
+    //
+    function onSuccess(position) {
+        //var element = document.getElementById('geolocation');
+        document.getElementById("x").value =  position.coords.latitude;
+        document.getElementById("y").value =  position.coords.longitude;
+                            
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    // Options: throw an error if no update is received every 30 seconds.
+    //
+    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 5000 });
