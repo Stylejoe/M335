0d7f55ad812f7b14681d05f45ad413c48f586ff7
Buchmann Christopher <chbuchmannch@gmail.com>
2017-02-24 08:49:58 +0100

    commit

---
+++ Tracker/platforms/browser/www/js/readData.js
@@ -0,0 +1,11 @@
+  
+  function authentificateUser()
+  {
+        var userId = firebase.auth().currentUser.uid;
+        return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+        var username = snapshot.val().username;
+  }
+
+  
+
+
---
+++ Tracker/www/js/writeData.js
@@ -0,0 +1,53 @@
+
+function latitudeAndLongitudeChanged(latitude, longitude)
+{
+    return true;
+}
+
+function setData()
+{
+    var latitude = document.getElementById("x").value;
+    var longitude = document.getElementById("y").value;
+    console.log("ELEMENTE GEHOLT");
+
+        if(latitude != null && longitude != null)
+        {       
+            console.log("ERSTES IF");     
+            if(latitudeAndLongitudeChanged(latitude, longitude))
+            {
+                console.log("IM IF DRINN");
+                writeHistoryData(latitude, longitude);
+            }
+
+        }
+}
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+
+
+function writeHistoryData(latitude, longitude)
+{
+    console.log("IM IF DRINN");
+
+    var locationHistory = {
+        uid: "uid",
+        latitude: latitude,
+        longitude: longitude
+    };
+
+    console.log("IM IF DRINN");
+
+    var newHistoryKey = firebase.database().ref().child('historys').push().key;
+
+    var updates = {};
+    updates['/historys/' + newHistoryKey] = locationHistory;
+
+    return firebase.database().ref().update(updates);
+}
